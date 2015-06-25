34991388155cd2f808de4a5ff2b343d80fb4156d
Jeffery Chen Fan <jeffery9@gmail.com>
2015-06-25 22:07:29 +0800

    百度地图

---
+++ baidu_map/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "replace google map with baidu map",
+
+    'summary': """
+        replace google map with baidu map for chinese user""",
+
+    'description': """
+         replace google map with baidu map for chinese user
+    """,
+
+    'author': 'Odoo CN, Jeffery <jeffery9@gmail.com>',
+    'website': "http://www.odoouse.cn",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base_geolocalize', 'website'],
+
+    'data': [
+
+        'templates.xml',
+    ],
+}
