3f9b5db46da88af9d1de56064857c8118efb2137
Jeffery Chen Fan <jeffery9@gmail.com>
2015-06-26 12:29:46 +0800

    地图默认缩放设置为10倍，公司地图为15倍

--- baidu_map/models.py
+++ baidu_map/models.py
@@ -12,7 +12,7 @@ class res_partner(models.Model):
     _inherit = "res.partner"
 
     @api.model
-    def baidu_map_img(self, zoom=8, width=298, height=298):
+    def baidu_map_img(self, zoom=10, width=298, height=298):
         '''
         http://api.map.baidu.com/staticimage?center=116.403874,39.914888&width=300&height=200&zoom=11
 
@@ -29,7 +29,7 @@ class res_partner(models.Model):
         return urlplus('http://api.map.baidu.com/staticimage', params)
 
     @api.model
-    def baidu_map_link(self, zoom=8):
+    def baidu_map_link(self, zoom=10):
         '''
      http://api.map.baidu.com/marker?location=40.047669,116.313082&title=我的位置&content=百度奎科大厦&output=html&src=yourComponyName|yourAppName    //调起百度PC或web地图，且在（lat:39.916979519873，lng:116.41004950566）坐标点上显示名称"我的位置"，内容"百度奎科大厦"的信息窗口。
         '''
@@ -48,9 +48,9 @@ class res_company(models.Model):
     _inherit = "res.company"
 
     @api.model
-    def baidu_map_img(self, zoom=10, width=298, height=298):
+    def baidu_map_img(self, zoom=15, width=298, height=298):
         return self.partner_id.baidu_map_img(zoom, width, height) or None
 
     @api.model
-    def baidu_map_link(self, zoom=8):
+    def baidu_map_link(self, zoom=10):
         return self.partner_id.baidu_map_link(zoom) or None
